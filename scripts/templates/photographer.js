9a0dbebfb0288ff2f21e679a6d5805f93e673f7f
david <davidcolmoulin974@gmail.com>
2023-11-28 12:53:01 +0400

    :wrench: Ajout object-fit et overflow sur '.photographer_section article img'

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -9,9 +9,18 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+
+        // ajout de la const P pour afficher le pays, la ville et la phrase 
+        // const p = document.createElement ( 'p' )
+        // P.textContent = tagline;
+        // article.appendChild(p);
+        
         article.appendChild(img);
-        article.appendChild(h2);
+        article.appendChild(h2); 
+
         return (article);
     }
-    return { name, picture, getUserCardDOM }
+    return {
+        //  name, picture,
+          getUserCardDOM }
 }
