c4129935db479300d84d1358867a3149f2b328e7
david <davidcolmoulin974@gmail.com>
2023-11-29 22:52:03 +0400

    :wrench: creation de 'p' pour afficher la ville et le pays + mise en css de 'photographer_villeText'

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,26 +1,45 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
+    const { name, portrait, tagline } = data;
 
     const picture = `assets/PhotographersIDPhotos/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
+       
         const img = document.createElement( 'img' );
         img.setAttribute("src", picture)
+        // ajout du setAttribute pour creer la class dans la DIV "photographers_section"
+        img.setAttribute("class", "photographer_picture")
+
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        // ajout du setAttribute pour creer la class dans la DIV "photographers_section"
+        h2.setAttribute("class", "photographer_name");
 
-        // ajout de la const P pour afficher le pays, la ville et la phrase 
-        // const p = document.createElement ( 'p' )
-        // P.textContent = tagline;
-        // article.appendChild(p);
+        // ajout de l'element "p'  pour afficher la ville et la region 
+        // const villeText = document.createElement ( 'p' );
+        // villeText.textContent = city + ", " + country;
+
+        // ajout du setAttribute pour creer la class dans la DIV "photographers_section"
+        // villeText.setAttribute("class", "photographer_villeText");
+        // article.appendChild(villeText);
+
+        // ajout de l'element "span" pour afficher le tagline 
+        const taglineText = document.createElement( 'span' );
+		taglineText.textContent = tagline;
+        // ajout du setAttribute pour creer la class dans la DIV "photographers_section"
+        taglineText.setAttribute("class", "photograper_taglineText")
+        article.appendChild(taglineText);
+			
         
         article.appendChild(img);
         article.appendChild(h2); 
 
+
         return (article);
     }
-    return {
-        //  name, picture,
-          getUserCardDOM }
+    return { name, picture,
+        //  city,
+        //  country,
+         tagline, getUserCardDOM }
 }
