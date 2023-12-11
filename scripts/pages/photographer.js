dcab3b2c99bc21df7c3d9a3519be9f6850f135d2
MD-974 <davidcolmoulin974@gmail.com>
2023-12-11 20:53:19 +0400

    :wrench: Mise en place de la recuperation et de l extraction d'un photographe via son 'id' dans 'photographers.js + recuperation de l'id' de l'url' pour un photographe ainsi que les datas de ce photographe dans 'onePhotographer.js'

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,6 +1,23 @@
 //Mettre le code JavaScript lié à la page photographer.html
 
-// Récupere l'id de l'url
-const paramsUrl = new URLSearchParams(window.location.search);
-const id = paramsUrl.get("id");
-console.log (id)
+async function getOnePhotographer(id) {
+    // recuperation des photographes
+    const reponsePhotographer = await fetch ("data/photographers.json");
+    const { photographers } = await reponsePhotographer.json();
+
+    // extraction d'un photographe par l id
+    let onePhotographe = undefined;
+    photographers.forEach(element => {
+        if (element.id == id) {
+            onePhotographe = element;
+        }  
+    });
+    
+// attention il faut gerer aussi les erreur d id 
+
+   return onePhotographe;
+   
+
+}
+
+getOnePhotographer();
--- scripts/templates/onePhotographer.js
+++ scripts/templates/onePhotographer.js
@@ -1,21 +1,43 @@
+async function displayData(photographer) {
+    const {city, country, name, tagline } = photographer;
 
-// Creation "div" informations
-const informations = document.createElement("div");
-informations.setAttribute("class", "onePhotographer_information");
+    // Creation "div" informations
+    const informations = document.createElement("div");
+    informations.setAttribute("class", "onePhotographer_information");
 
-const h2 = document.createElement("h2");
-h2.textContent = name;
-h2.setAttribute("class", "onePhotographer_nom");
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    h2.setAttribute("class", "onePhotographer_nom");
 
-const villeText = document.createElement("p");
-villeTextText.textContent = city + "," + country;
-villeText.setAttribute("class", "onePhotographer_ville");
+    const villeText = document.createElement("p");
+    villeText.textContent = city + "," + country;
+    villeText.setAttribute("class", "onePhotographer_ville");
 
-const taglineText = document.createElement("p");
-taglineText.textContent = tagline;
-taglineText.setAttribute("class", "onePhotographer_tagline");
+    const taglineText = document.createElement("p");
+    taglineText.textContent = tagline;
+    taglineText.setAttribute("class", "onePhotographer_tagline");
 
-// ajout de article.appendChild pour l'affichage dans l'ordre des elements
-informations.appendChild(h2);
-informations.appendChild(villeText);
-informations.appendChild(taglineText);
+    // ajout de article.appendChild pour l'affichage dans l'ordre des elements
+    informations.appendChild(h2);
+    informations.appendChild(villeText);
+    informations.appendChild(taglineText);
+
+    const onePhotographerSection = document.querySelector(".photograph-header");
+    onePhotographerSection.appendChild(informations);
+
+}
+   
+
+async function init() {
+    // Récuperation de l'id de l'url
+    const paramsUrl = new URLSearchParams(window.location.search);
+    const id = paramsUrl.get("id");
+    console.log (id);
+
+    // Récupère les datas d'un photographe
+    const dataOnePhotographe  = await getOnePhotographer(id);
+    console.log(dataOnePhotographe)
+    displayData(dataOnePhotographe);
+}
+
+init();
