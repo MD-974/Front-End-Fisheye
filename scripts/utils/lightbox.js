244a82cac198825e39d9d8720bb96d43121af1da
MD-974 <davidcolmoulin974@gmail.com>
2024-01-20 16:53:21 +0400

    :recycle:refactor 'lightbox.js'

--- scripts/utils/lightbox.js
+++ scripts/utils/lightbox.js
@@ -1,8 +1,10 @@
+//----- AFFICHER LIGHTBOX ----
 function displayLightbox() {
     const lightbox = document.getElementById("lightbox");
 	lightbox.style.display = "block";
 }
 
+//----- FERMER LIGHTBOX ----
 function closeLightbox() {
     const lightbox = document.getElementById("lightbox");
     lightbox.style.display = "none";
