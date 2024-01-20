26c305bc29178f67147ed8e9d58c89f9f08e1980
MD-974 <davidcolmoulin974@gmail.com>
2024-01-20 16:28:53 +0400

    :recycle:refactor 'mediaCardFactory.js'

--- scripts/factories/mediaCardFactory.js
+++ scripts/factories/mediaCardFactory.js
@@ -1,101 +1,120 @@
+/**
+ * Fonction crée une carte média pour afficher 
+ * les images ou vidéos d'un photographe
+ * avec leurs informations associées (titre, likes, coeur).
+ * @param {Object} media - Les informations sur le média à afficher (image ou vidéo).
+ * @param {Array} objPhotographerMedias - La liste des médias du photographe.
+ * @returns {Object} - Retourne la carte média créée.
+*/
+
 function mediaCardFactory (media,objPhotographerMedias ) {
-    // div conteneur "contient les informations (image,titre,like,coeur) 
-    // pour chaques medias (images/videos) d'un photographe"
-    const mediasCard = document.createElement ("div");
-    mediasCard.setAttribute("class", "onephotographer_media-card");
-    
-
-    if (media.image) {
-        const mediasImage = document.createElement ("img");
-        const urlImage = `assets/medias/${media.image}`;
-        mediasImage.setAttribute("src", urlImage);
-        mediasImage.setAttribute("class", "onephotographer_imageMedias");
-        mediasImage.setAttribute("width", "350px");
-        mediasImage.setAttribute("height", "300px");
-        mediasImage.style.borderRadius = "5px";
-        mediasImage.style.objectFit = "cover";
-        mediasCard.append(mediasImage);
+ // Création de la carte média
+ const mediasCard = document.createElement ("div");
+ mediasCard.setAttribute("class", "onephotographer_media-card");
+   
+ 
+ // Création de l'image si c'est une image,
+ //ou de la vidéo si c'est une vidéo
+ if (media.image) {
+     // Création de l'élément image
+     const mediasImage = document.createElement ("img");
+     // Définition de l'URL de l'image
+     const urlImage = `assets/medias/${media.image}`;
+     mediasImage.setAttribute("src", urlImage);
+     mediasImage.setAttribute("class", "onephotographer_imageMedias");
+     mediasImage.setAttribute("width", "350px");
+     mediasImage.setAttribute("height", "300px");
+     mediasImage.style.borderRadius = "5px";
+     mediasImage.style.objectFit = "cover";
+     mediasCard.append(mediasImage);
     } else if (media.video) {
-        const mediasVideo = document.createElement ("video");
-        mediasVideo.setAttribute("controls", "controls");
-        mediasVideo.setAttribute("class", "onephotographer_imageMedias");
-        mediasVideo.setAttribute("width", "350px");
-        mediasVideo.setAttribute("height", "300px");
-        mediasVideo.style.borderRadius = "5px";
-        mediasVideo.style.objectFit = "cover";
-        const videoSource = document.createElement ("source");
-        const urlVideo = `assets/medias/${media.video}`;
-        videoSource.setAttribute("src", urlVideo);
-        videoSource.setAttribute("type", "video/mp4");
-        mediasVideo.append(videoSource);
-        mediasCard.append(mediasVideo);
+     // Création de l'élément video
+     const mediasVideo = document.createElement ("video");
+     mediasVideo.setAttribute("controls", "controls");
+     mediasVideo.setAttribute("class", "onephotographer_imageMedias");
+     mediasVideo.setAttribute("width", "350px");
+     mediasVideo.setAttribute("height", "300px");
+     mediasVideo.style.borderRadius = "5px";
+     mediasVideo.style.objectFit = "cover";
+     // Création de la source vidéo
+     const videoSource = document.createElement ("source");
+     // Définition de l'URL de la vidéo
+     const urlVideo = `assets/medias/${media.video}`;
+     videoSource.setAttribute("src", urlVideo);
+     videoSource.setAttribute("type", "video/mp4");
+     mediasVideo.append(videoSource);
+     mediasCard.append(mediasVideo);
     }
 
-    const mediasInfos = document.createElement ("div");
-    mediasInfos.setAttribute("class", "onephotographer_medias-infos");
-    mediasInfos.style.display = "flex";
-    mediasInfos.style.alignItems = "center";
-    mediasInfos.style.justifyContent = "space-between"
+ // Création de la div qui contient les informations du media
+ const mediasInfos = document.createElement ("div");
+ mediasInfos.setAttribute("class", "onephotographer_medias-infos");
+ mediasInfos.style.display = "flex";
+ mediasInfos.style.alignItems = "center";
+ mediasInfos.style.justifyContent = "space-between"
 
-    // element "titre du media(image ou video)"   
-    const mediasTitre = document.createElement ("h3");
-    mediasTitre.textContent = media.title;
-    mediasInfos.appendChild(mediasTitre);
+ // Création "Titre du media(image ou video)"   
+ const mediasTitre = document.createElement ("h3");
+ mediasTitre.textContent = media.title;
+ mediasInfos.appendChild(mediasTitre);
 
-    const mediasLikeWrapper = document.createElement ("div");
-    mediasLikeWrapper.setAttribute("class", "onephotographer_medias-likes-wrapper");
-    mediasLikeWrapper.style.display = "flex";
-    mediasLikeWrapper.style.gap ="10px";
+ // Création du wrapper des likes du média
+ const mediasLikeWrapper = document.createElement ("div");
+ mediasLikeWrapper.setAttribute("class", "onephotographer_medias-likes-wrapper");
+ mediasLikeWrapper.style.display = "flex";
+ mediasLikeWrapper.style.gap ="10px";
     
-    // element "like du media(image ou video)"
-    const mediaslike = document.createElement ("p");
-    mediaslike.textContent = media.likes;
-    mediasLikeWrapper.appendChild(mediaslike);
+ // Création "likes du media(image ou video)"
+ const mediaslike = document.createElement ("p");
+ mediaslike.textContent = media.likes;
+ mediasLikeWrapper.appendChild(mediaslike);
 
-    // element "coeur du media(image ou video)"
-    const mediasCoeur = document.createElement ("i");
-    if (media.islike) {
-        mediasCoeur.setAttribute("class", "fa-regular fa-heart fas" );
+ // Création "coeur du media(image ou video)"
+ const mediasCoeur = document.createElement ("i");
+ if (media.islike) {
+     mediasCoeur.setAttribute("class", "fa-regular fa-heart fas" );
     }else {
-        mediasCoeur.setAttribute("class", "fa-regular fa-heart far" );
+     mediasCoeur.setAttribute("class", "fa-regular fa-heart far" );
     }
-
-    mediasCoeur.style.color = "#901C1C";
-    mediasLikeWrapper.appendChild(mediasCoeur);
+ mediasCoeur.style.color = "#901C1C";
+ mediasLikeWrapper.appendChild(mediasCoeur);
     
     
-    //  Ajout d'un eventlistener sur le coeur plus incrementation et decrementation du like
-    mediasCoeur.addEventListener("click", function() {
-        if (mediasCoeur.classList.contains("fas")) {
-            mediasCoeur.classList.remove("fas");
-            mediasCoeur.classList.add("far");
-            media.likes = media.likes - 1;
-            media.islike = false
-            mediaslike.textContent = media.likes;
-            console.log("vous avez dislike ce media : " + media.title);
+ // Ajout d'un event listener sur le coeur pour incrémenter/décrémenter le like
+ mediasCoeur.addEventListener("click", function() {
+     if (mediasCoeur.classList.contains("fas")) {
+         mediasCoeur.classList.remove("fas");
+         mediasCoeur.classList.add("far");
+         // Diminution du nombre de likes
+         media.likes = media.likes - 1;
+         // Changement de l'attribut
+         media.islike = false
+         mediaslike.textContent = media.likes;
+         console.log("vous avez dislike ce media : " + media.title);
         } else {
-            mediasCoeur.classList.remove("far");
-            mediasCoeur.classList.add("fas");
-            media.likes = media.likes + 1;
-            media.islike = true
-            mediaslike.textContent = media.likes;  
-            console.log("vous avez like ce media : " + media.title); 
+         mediasCoeur.classList.remove("far");
+         mediasCoeur.classList.add("fas");
+         // Augmentation du nombre de likes
+         media.likes = media.likes + 1;
+         // Changement de l'attribut
+         media.islike = true
+         mediaslike.textContent = media.likes;  
+         console.log("vous avez like ce media : " + media.title); 
         }
-
-        let compteur = 0
-        objPhotographerMedias.forEach(element => {
+     // Calcul du total de likes
+     let compteur = 0
+     // Boucle de calcul du total de likes
+     objPhotographerMedias.forEach(element => {
          compteur = compteur + Number(element.likes)  
         });
-        console.log( "Total de like : " + compteur)
-        
-        
-        document.querySelector("#compteur-likes").innerHTML = compteur
-
+     console.log( "Total de like : " + compteur)
+     // Mise à jour du compteur 
+     document.querySelector("#compteur-likes").innerHTML = compteur
     });
 
+ // Ajout des elements dans la div du media
+ mediasCard.appendChild(mediasInfos);
+ mediasInfos.appendChild(mediasLikeWrapper)
 
-    mediasCard.appendChild(mediasInfos);
-    mediasInfos.appendChild(mediasLikeWrapper)
-
-    return {mediasCard }
+ return {mediasCard }
 }
